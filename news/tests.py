b661ba09fc33ba2095a81c6c0c352c40294913b5
cherucole <cheruyotcheruyot@gmail.com>
2018-09-24 16:39:00 +0300

    created one test and passing it

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+#instead of importing items/classes one by one use * to import everything
+from .models import *
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.cheruiyot=Editor(first_name='Cheruiyot', last_name='Collins', email='noreply@gmail.com')
+
+#testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cheruiyot,Editor))
