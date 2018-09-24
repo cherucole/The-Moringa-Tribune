b39b367b18fef209d3356d270767923f60210215
cherucole <cheruyotcheruyot@gmail.com>
2018-09-24 14:46:02 +0300

    added previous dates

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from django.http import HttpResponse,Http404
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def welcome(request):
@@ -8,43 +8,31 @@ def welcome(request):
 
 def news_of_day(request):
     date=dt.date.today()
-    #function to convert date object to find the exact day
-    day=convert_dates(date)
-    html=f'''
-    <html>
-    <body>
-    <h1> News for {day} {date.day}--{date.month}--{date.year}</h1>
-    </body>
-    </html>
-    '''
-    return HttpResponse(html)
 
-def convert_dates(dates):
-    #function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-    days =['Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday','Sunday']
+    return render(request, 'all-news/today-news.html', {"date":date,})
 
-    #returning the actual day of the week
-    day = days[day_number]
-    return day
+# def convert_dates(dates):
+#     #function that gets the weekday number for the date.
+#     day_number = dt.date.weekday(dates)
+#     days =['Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday','Sunday']
+#
+#     #returning the actual day of the week
+#     day = days[day_number]
+#     return day
 
 def past_days_news(request,past_date):
 
     try:
 
         #converts data from string url
-        date = dt.datetime.strptime(past_date,'%y-%m-%d').date()
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
     except ValueError:
         #Raise 404 error when valueError is thrown
         raise Http404()
+        assert False
 
-    day=convert_dates(date)
-    html=f'''
-    <html>
-    <body>
-    <h1> News for {day} {date.day} - {date.month} - {date.year}</h1>
-    </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    if date==dt.datetime.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date":date})
