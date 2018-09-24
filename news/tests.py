f3f5bc47ce58a0cf8ff59a77f66902b31570004a
cherucole <cheruyotcheruyot@gmail.com>
2018-09-24 17:26:37 +0300

    working admin with adding admin also

--- news/tests.py
+++ news/tests.py
@@ -11,3 +11,30 @@ class EditorTestClass(TestCase):
 #testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.cheruiyot,Editor))
+
+
+    #testing the save method
+    def test_save_method(self):
+        self.cheruiyot.save_editor()
+        editors=Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+    def tearDown(self):
+
+        # Editor.objects_all = []
+
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    #
+    # def test_delete_object(self):
+    #     '''
+    #     this is to ensure we can remove a social account from our list
+    #     '''
+    #     self.cheruiyot.save_editor()
+    #     test_object = Editor(first_name='Cheruiyot', last_name='Collins', email='noreply@gmail.com') #new account to save
+    #     test_object.save_editor()
+    #
+    #     self.test_object.delete_object()
+    #     self.assertEqual(len(Editor.objects_all),1)
