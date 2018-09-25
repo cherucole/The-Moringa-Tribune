704be0cb35c9a643032d7f99ddc1f4e5c3f71c87
cherucole <cheruyotcheruyot@gmail.com>
2018-09-25 15:22:16 +0300

    try 200

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ju6d%$!cai#fn^e(9-8=@@#hxq0$(d^(7xu9l$(n!kna0hx!^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
