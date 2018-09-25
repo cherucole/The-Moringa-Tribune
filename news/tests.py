8dfc62c5592134e8e269049fb5f023f6292af406
cherucole <cheruyotcheruyot@gmail.com>
2018-09-25 10:37:37 +0300

    4 tests that all pass

--- news/tests.py
+++ news/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 #instead of importing items/classes one by one use * to import everything
 from .models import *
+import datetime as dt
 
-# Create your tests here.
+#Create your tests here.
 class EditorTestClass(TestCase):
     #setup method
     def setUp(self):
@@ -26,6 +27,37 @@ class EditorTestClass(TestCase):
         Editor.objects.all().delete()
         tags.objects.all().delete()
         Article.objects.all().delete()
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james = Editor(first_name='James', last_name='Muriuki', email='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name='testing')
+        self.new_tag.save()
+
+        self.new_article = Article(title='Test Article', post='This is a random test Post', editor=self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news) > 0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
 
     #
     # def test_delete_object(self):
