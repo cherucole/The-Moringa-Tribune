6db5f80e99389a3e067b6deffc01ed2ac3b78905
cherucole <cheruyotcheruyot@gmail.com>
2018-10-10 10:29:34 +0300

    successfully adding to mail list works

--- news/views.py
+++ news/views.py
@@ -32,14 +32,14 @@ def news_today(request):
     #     form=NewsLetterForm()
     return render(request, 'all-news/todays-news.html', {"date": date, "news": news, "letterForm":form})
 
-
 def newsletter(request):
-    name=request.POST.get('your name')
-    email=request.POST.get('email')
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+
     recipient = NewsLetterRecipients(name=name, email=email)
     recipient.save()
     send_welcome_email(name, email)
-    data = {'success': 'You have been successfully added to the mailing list '}
+    data = {'success': 'You have been successfully added to mailing list'}
     return JsonResponse(data)
 
 def past_days_news(request, past_date):
